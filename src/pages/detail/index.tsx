2240a042e92ce13f2df64fda93efcf8a345fe4d1
jpwieland <31457537+jpwieland@users.noreply.github.com>
2024-04-25 23:35:28 -0300

    ïnitial commit

---
+++ src/pages/detail/index.tsx
@@ -0,0 +1,12 @@
+import {View,Text, TouchableOpacity} from 'react-native'
+
+const Detail = () => {
+    return (
+        <TouchableOpacity>
+            <Text>Detail </Text>
+        </TouchableOpacity>
+    )
+
+}
+
+export default Detail;
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,12 @@
+import {View,Text, TouchableOpacity} from 'react-native'
+
+const Home = () => {
+    return (
+        <TouchableOpacity>
+            <Text>Detail </Text>
+        </TouchableOpacity>
+    )
+
+}
+
+export default Home;
